3d2d96ce6dfd56614a30ef45757fb8f176586c4e
Luca1008 <111925104+Luca1008@users.noreply.github.com>
2023-08-27 17:01:16 -0300

    asd
    asd

---
+++ TP1/src/packeage1/BBDD.java
@@ -0,0 +1,7 @@
+packeage packeage1
+
+public class BBDD {
+    public static final String DATABASE_NAME = "packeage"; // Nombre de la base de datos.
+    public static final int DATABASE_VERSION = 2; // Versión actual de la base de datos.
+
+}
