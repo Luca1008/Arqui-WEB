a7ab64a9c829bee87178da2fb48cc01f92b08478
Luca1008 <111925104+Luca1008@users.noreply.github.com>
2023-10-10 13:43:09 -0300

    Funciona punto 2.F

--- demo/src/main/java/com/example/demo/dtos/DtoCarrera.java
+++ demo/src/main/java/com/example/demo/dtos/DtoCarrera.java
@@ -5,10 +5,11 @@ import lombok.Getter;
 @Getter
 public class DtoCarrera {
 	private String nombre;
-	private int Cant;
+	private Long Cant;
 
 	public DtoCarrera(Object resultado, Object resultado2) {
 		this.nombre = (String) resultado;
-		this.Cant = (int) resultado2;
+		this.Cant = (Long) resultado2;
 	}
 }
+
