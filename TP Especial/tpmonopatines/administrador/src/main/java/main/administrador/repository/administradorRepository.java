8a56f3f222e73d323027ccbc1a9c34731e26a5ea
TuzioDj <90795509+TuzioDj@users.noreply.github.com>
2023-10-26 12:39:54 -0300

    nashe

--- TP Especial/tpmonopatines/administrador/src/main/java/main/administrador/repository/administradorRepository.java
+++ TP Especial/tpmonopatines/administrador/src/main/java/main/administrador/repository/administradorRepository.java
@@ -4,7 +4,7 @@ import main.administrador.model.administrador;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface cuentaRepository extends JpaRepository<cuenta, Long> {
+public interface administradorRepository extends JpaRepository<administrador, Long> {
     // Aquí puedes poner tus consultas personalizadas a la base de datos
     
 }
